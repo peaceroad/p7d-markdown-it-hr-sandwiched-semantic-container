61a5da6a026dd293dd9198ff7440622c4e8aa1cb
k_taka <k_taka@gihyo.co.jp>
2021-07-10 13:34:03 +0900

    update.

--- index.js
+++ index.js
@@ -5,164 +5,136 @@ module.exports = function semantic_container_plugin(md) {
   const semantics = require('./semantics.json');
   const semanticsJoint = '[:.　：。．]';
 
-  function semantic_container (state, startLine, endLine, silent) {
-    let n = 1;
-    while (n < state.tokens.length -2) {
-      const token = state.tokens[n];
-      const prevToken = state.tokens[n-1];
-      const nextToken = state.tokens[n+1];
+  const checkSematicContainerCore = (state, n, hrType, sc, checked) => {
 
-      if (prevToken.type !== 'hr') { n++; continue; }
-      let hrMarkup = '';
-      if(/\*/.test(prevToken.markup)) hrMarkup = '*';
-      if(/-/.test(prevToken.markup)) hrMarkup = '-';
-      if(/_/.test(prevToken.markup)) hrMarkup = '_';
+    const nextToken = state.tokens[n+1];
 
-      let sn = 0;
-      let hasSemantics = false;
-      let actualSemantics = null;
-      while (sn < semantics.length) {
-        let semanticsAltRegStr = '';
-        if(semantics[sn].as) {
-          const ts = semantics[sn].as.split(',');
-          ts.forEach(x => {
-            semanticsAltRegStr += '|' + x.trim();
-          });
+    let sn = 0;
+    let actualName = null;
+    while (sn < semantics.length) {
+      let semanticsAltRegStr = '';
+      if(semantics[sn].as) {
+        const ts = semantics[sn].as.split(',');
+        ts.forEach(x => {
+          semanticsAltRegStr += '|' + x.trim();
+        });
+      }
+      actualName = nextToken.content.match(new RegExp('(?:' + semantics[sn].name + semanticsAltRegStr + ')' + semanticsJoint, 'i'));
+      if(actualName) break;
+      sn++;
+    }
+    if(!actualName) { return false; }
+
+    const actualNameJoint = actualName[0].match(new RegExp(semanticsJoint + '$'));
+
+    let en = n;
+    let hasEndSemanticsHr = false;
+    while (en < state.tokens.length) {
+      const eToken = state.tokens[en];
+      if (eToken.type !== 'hr') { en++; continue; }
+      if (new RegExp('\\' + hrType).test(eToken.markup)) {
+        hasEndSemanticsHr = true;
+        break;
+      }
+      en++;
+    }
+    if(!hasEndSemanticsHr) { return false; }
+
+    sc.push({
+      "range": [n, en],
+      "continued": checked,
+      "sn": sn,
+      "actualName": actualName[0],
+      "actualNameJoint": actualNameJoint[0]
+    });
+
+
+    return true;
+  };
+
+
+  const checkSemanticContainer = (state, n, hrType, sc) => {
+    let continued = false;
+    if (!checkSematicContainerCore(state, n, hrType, sc, continued)) { 
+      return false;
+    }
+    //console.log(sc[sc.length - 1].range[1] + 1);
+    let cn = sc[sc.length - 1].range[1] + 1; // <hr>:2, </p>:1
+    while (cn < state.tokens.length -1) {
+      //console.log('next cn: ' + cn);
+      continued = true;
+      if (!checkSematicContainerCore(state, cn, hrType, sc, continued)) {
+        return true;
+      }
+      cn = sc[sc.length - 1].range[1];
+    }
+
+    return true;
+  };
+
+
+  const setSemanticContainer = (state, n, hrType, sc) => {
+    let moveToAriaLabel = false;
+    const rs = sc.range[0];
+    const re = sc.range[1];
+    const sn = sc.sn;
+    const nextToken = state.tokens[rs+1];
+
+    const sToken = new state.Token('html_block', '', 0);
+    sToken.content = '<' + semantics[sn].tag;
+    sToken.content += ' class="' + semantics[sn].name + '"';
+    if (semantics[sn].attrs.length > 0) {
+      let ai = 0;
+      while (ai < semantics[sn].attrs.length) {
+        if( semantics[sn].attrs[ai][0]=== "aria-label" && semantics[sn].attrs[ai][1]=== "NAME") {
+          semantics[sn].attrs[ai][1] = sc.actualName.replace(new RegExp('\\' + sc.actualNameJoint + '$'), '');
+          moveToAriaLabel = true;
         }
-        actualSemantics = nextToken.content.match(new RegExp('(?:' + semantics[sn].name + semanticsAltRegStr + ')' + semanticsJoint, 'i'));
-        if(actualSemantics) break;
-        sn++;
+        sToken.content += ' ' + semantics[sn].attrs[ai][0] + '="' + semantics[sn].attrs[ai][1] + '"';
+        ai++;
       }
-      if(!actualSemantics) { n++; continue; }
+    }
+    sToken.content += '>\n';
+    sToken.block = true;
+    state.tokens.splice(rs, 0, sToken);
 
-      const actualSemanticsJoint = actualSemantics[0].match(new RegExp(semanticsJoint + '$'));
+    const eToken = new state.Token('html_block', '', 0);
+    eToken.content = '</' + semantics[sn].tag + '>\n';
+    eToken.block = true;
+    state.tokens.splice(re+1, 1, eToken); // ending hr delete too.
+    if (!sc.continued) {
+      state.tokens.splice(rs-1, 1)// starting hr delete.
+    }
 
-      let en = n;
-      let hasEndSemanticsHr = false;
-      while (en < state.tokens.length) {
-        const eToken = state.tokens[en];
-        if (eToken.type !== 'hr') { en++; continue; }
-        if (new RegExp('\\' + hrMarkup).test(eToken.markup)) {
-          hasEndSemanticsHr = true;
-          break;
-        }
-        en++;
-      }
-      if(!hasEndSemanticsHr) { n++; continue; }
+    if(moveToAriaLabel) {
+      //sn++;
+      nextToken.content = nextToken.content.replace(new RegExp('^' + sc.actualName + ' *'), '');
+      nextToken.children[0].content = nextToken.children[0].content.replace(new RegExp('^' + sc.actualName + ' *'), '');
+      return;
+    }
 
-
-
-      let moveToAriaLabel = false;
-      const t_start = new state.Token('html_block', '', 0);
-      t_start.content = '<' + semantics[sn].tag.trim();
-      t_start.content += ' class="' + semantics[sn].name + '"';
-      if (semantics[sn].attrs.length > 0) {
-        let ai = 0;
-        while (ai < semantics[sn].attrs.length) {
-          if( semantics[sn].attrs[ai][0]=== "aria-label" && semantics[sn].attrs[ai][1]=== "NAME") {
-            semantics[sn].attrs[ai][1] = actualSemantics[0].replace(new RegExp('\\' + actualSemanticsJoint[0] + '$'), '');
-            moveToAriaLabel = true;
-          }
-          t_start.content += ' ' + semantics[sn].attrs[ai][0] + '="' + semantics[sn].attrs[ai][1] + '"';
-          ai++;
-        }
-      }
-      t_start.content += '>\n';
-      t_start.block = true;
-      state.tokens.splice(n, 0, t_start);
-
-      const t_end = new state.Token('html_block', '', 0);
-      t_end.content = '</' + semantics[sn].tag.trim() + '>\n';
-      t_end.block = true;
-      state.tokens.splice(en+1, 1, t_end); // ending hr delete too.
-
-      state.tokens.splice(n-1, 1)// starting hr delete.
-
-
-      if(moveToAriaLabel) {
-        n++;
-        nextToken.content = nextToken.content.replace(new RegExp('^' + actualSemantics[0] + ' *'), '');
-        nextToken.children[0].content = nextToken.children[0].content.replace(new RegExp('^' + actualSemantics[0] + ' *'), '');
-        continue;
-      }
-
-      if (/^[*_]{1,2}/.test(nextToken.content)) {
-        nextToken.children[1].attrJoin("class", semantics[sn].name + '-label');
-      } else {
-        const lt_first = {
-          type: 'text',
-          tag: '',
-          attrs: null,
-          map: null,
-          nesting: 0,
-          level: 0,
-          children: null,
-          content: '',
-          markup: '',
-          info: '',
-          meta: null,
-          block: false,
-          hidden: false
-        };
-        const lt_span_open = {
-          type: 'span_open',
-          tag: 'span',
-          attrs: [["class", semantics[sn].name + "-label"]],
-          map: null,
-          nesting: 1,
-          level: 0,
-          children: null,
-          content: '',
-          markup: '',
-          info: '',
-          meta: null,
-          block: false,
-          hidden: false
-        };
-        const lt_span_content = {
-          type: 'text',
-          tag: '',
-          attrs: null,
-          map: null,
-          nesting: 0,
-          level: 1,
-          children: null,
-          content: actualSemantics[0],
-          markup: '',
-          info: '',
-          meta: null,
-          block: false,
-          hidden: false
-        };
-        const lt_span_close = {
-          type: 'span_close',
-          tag: 'span',
-          attrs: null,
-          map: null,
-          nesting: -1,
-          level: 0,
-          children: null,
-          content: '',
-          markup: '',
-          info: '',
-          meta: null,
-          block: false,
-          hidden: false
-        };
-        nextToken.children[0].content = nextToken.children[0].content.replace(actualSemantics[0], '');
-        nextToken.children.unshift(lt_span_close);
-        nextToken.children.unshift(lt_span_content);
-        nextToken.children.unshift(lt_span_open);
-        nextToken.children.unshift(lt_first);
-      }
-
-      // Add label joint span.
-      nextToken.children[2].content = nextToken.children[2].content.replace(new RegExp(actualSemanticsJoint[0] + '$'), '');
-
-      const ljt_span_open = {
+    if (/^[*_]{1,2}/.test(nextToken.content)) {
+      nextToken.children[1].attrJoin("class", semantics[sn].name + '-label');
+    } else {
+      const lt_first = {
+        type: 'text',
+        tag: '',
+        attrs: null,
+        map: null,
+        nesting: 0,
+        level: 0,
+        children: null,
+        content: '',
+        markup: '',
+        info: '',
+        meta: null,
+        block: false,
+        hidden: false
+      };
+      const lt_span_open = {
         type: 'span_open',
         tag: 'span',
-        attrs: [["class", semantics[sn].name + "-label-joint"]],
+        attrs: [["class", semantics[sn].name + "-label"]],
         map: null,
         nesting: 1,
         level: 0,
@@ -174,7 +146,7 @@ module.exports = function semantic_container_plugin(md) {
         block: false,
         hidden: false
       };
-      const ljt_span_content = {
+      const lt_span_content = {
         type: 'text',
         tag: '',
         attrs: null,
@@ -182,14 +154,14 @@ module.exports = function semantic_container_plugin(md) {
         nesting: 0,
         level: 1,
         children: null,
-        content: actualSemanticsJoint[0],
+        content: sc.actualName,
         markup: '',
         info: '',
         meta: null,
         block: false,
         hidden: false
       };
-      const ljt_span_close = {
+      const lt_span_close = {
         type: 'span_close',
         tag: 'span',
         attrs: null,
@@ -204,14 +176,96 @@ module.exports = function semantic_container_plugin(md) {
         block: false,
         hidden: false
       };
-      nextToken.children.splice(3, 0, ljt_span_close);
-      nextToken.children.splice(3, 0, ljt_span_content);
-      nextToken.children.splice(3, 0, ljt_span_open);
+      nextToken.children[0].content = nextToken.children[0].content.replace(sc.actualName, '');
+      nextToken.children.unshift(lt_span_close);
+      nextToken.children.unshift(lt_span_content);
+      nextToken.children.unshift(lt_span_open);
+      nextToken.children.unshift(lt_first);
+    }
 
-       n++;
+    // Add label joint span.
+    nextToken.children[2].content = nextToken.children[2].content.replace(new RegExp(sc.actualNameJoint + '$'), '');
+
+    const ljt_span_open = {
+      type: 'span_open',
+      tag: 'span',
+      attrs: [["class", semantics[sn].name + "-label-joint"]],
+      map: null,
+      nesting: 1,
+      level: 0,
+      children: null,
+      content: '',
+      markup: '',
+      info: '',
+      meta: null,
+      block: false,
+      hidden: false
+    };
+    const ljt_span_content = {
+      type: 'text',
+      tag: '',
+      attrs: null,
+      map: null,
+      nesting: 0,
+      level: 1,
+      children: null,
+      content: sc.actualNameJoint,
+      markup: '',
+      info: '',
+      meta: null,
+      block: false,
+      hidden: false
+    };
+    const ljt_span_close = {
+      type: 'span_close',
+      tag: 'span',
+      attrs: null,
+      map: null,
+      nesting: -1,
+      level: 0,
+      children: null,
+      content: '',
+      markup: '',
+      info: '',
+      meta: null,
+      block: false,
+      hidden: false
+    };
+    nextToken.children.splice(3, 0, ljt_span_close);
+    nextToken.children.splice(3, 0, ljt_span_content);
+    nextToken.children.splice(3, 0, ljt_span_open);
+
+    return;
+  };
+
+
+  function semanticContainer (state, startLine, endLine, silent) {
+
+    let n = 1;
+    while (n < state.tokens.length -1) {
+      //console.log(n + ": " + state.tokens[n].type + ": ==================================");
+      let sc = [];
+      let hrType = '';
+
+      const prevToken = state.tokens[n-1];
+
+      if (prevToken.type !== 'hr') { n++; continue; }
+      if(/\*/.test(prevToken.markup)) hrType = '*';
+      if(/-/.test(prevToken.markup)) hrType = '-';
+      if(/_/.test(prevToken.markup)) hrType = '_';
+
+      if (!checkSemanticContainer(state, n, hrType, sc)) { n++; continue; }
+      // console.log('set sc: '+ JSON.stringify(sc));
+
+      let sci = 0;
+      while (sci < sc.length) {
+        setSemanticContainer(state, n, hrType, sc[sci]);
+        sci++;
+      }
+      n++; //if "n = sc[sc.length - 1].range[1] + 1;", the inner semantic container is not processed.
     }
     return true;
   }
 
-  md.core.ruler.after('inline', 'semantic_container', semantic_container);
+  md.core.ruler.after('inline', 'semantic_container', semanticContainer);
 };
--- test/test.js
+++ test/test.js
@@ -4,19 +4,28 @@ const sc = require('../index.js');
 md.use(sc);
 
 const ms = [
-  [
+ /* [
     'A paragraph.\n\n* * *\n\nNotice. A notice.\n\n* * *\n\nA paragraph.',
     '<p>A paragraph.</p>\n<section class="notice" role="doc-notice">\n<p><span class="notice-label">Notice<span class="notice-label-joint">.</span></span> A notice.</p>\n</section>\n<p>A paragraph.</p>\n'
   ], [
     'A paragraph.\n\n* * *\n\n**Notice.** A notice.\n\n* * *\n\nA paragraph.',
     '<p>A paragraph.</p>\n<section class="notice" role="doc-notice">\n<p><strong class="notice-label">Notice<span class="notice-label-joint">.</span></strong> A notice.</p>\n</section>\n<p>A paragraph.</p>\n'
-  ], [
+  ],  [
     '# Title\n\nA paragraph.\n\n- - -\n\n## Column: Title\n\nA column.\n\n- - -\n\nA paragraph.',
     '<h1>Title</h1>\n<p>A paragraph.</p>\n<aside class="column">\n<h2><span class="column-label">Column<span class="column-label-joint">:</span></span> Title</h2>\n<p>A column.</p>\n</aside>\n<p>A paragraph.</p>\n'
   ], [
     '# A heading.\n\n* * *\n\nLead. A lead.\n\n* * *\n\nA paragraph.',
     '<h1>A heading.</h1>\n<section class="lead" aria-label="Lead">\n<p>A lead.</p>\n</section>\n<p>A paragraph.</p>\n'
-  ]
+  ], */[
+    '# Title\n\nA paragraph.\n\n- - -\n\n## Column: Title\n\nA column.\n\n___\n\nNotice. A column notice.\n\n___\n\nA column.\n\n- - -\n\nA paragraph.',
+    '<h1>Title</h1>\n<p>A paragraph.</p>\n<aside class="column">\n<h2><span class="column-label">Column<span class="column-label-joint">:</span></span> Title</h2>\n<p>A column.</p>\n<section class="notice" role="doc-notice">\n<p><span class="notice-label">Notice<span class="notice-label-joint">.</span></span> A column notice.</p>\n</section>\n<p>A column.</p>\n</aside>\n<p>A paragraph.</p>\n'
+  ], /*[
+    'A paragraph.\n\n* * *\n\nNotice. A notice.\n\n* * *\n\nNotice. A notice.\n\n* * *\n\nA paragraph.',
+    '<p>A paragraph.</p>\n<section class="notice" role="doc-notice">\n<p><span class="notice-label">Notice<span class="notice-label-joint">.</span></span> A notice.</p>\n</section>\n<section class="notice" role="doc-notice">\n<p><span class="notice-label">Notice<span class="notice-label-joint">.</span></span> A notice.</p>\n</section>\n<p>A paragraph.</p>\n'
+  ], [
+    'A paragraph.\n\n* * *\n\nNotice. A notice.\n\n* * *\n\n* * *\n\nNotice. A notice.\n\n* * *\n\nA paragraph.',
+    '<p>A paragraph.</p>\n<section class="notice" role="doc-notice">\n<p><span class="notice-label">Notice<span class="notice-label-joint">.</span></span> A notice.</p>\n</section>\n<section class="notice" role="doc-notice">\n<p><span class="notice-label">Notice<span class="notice-label-joint">.</span></span> A notice.</p>\n</section>\n<p>A paragraph.</p>\n'
+  ]*/
 ];
 
 
