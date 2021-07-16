8772c2cdbc2171cbd8eba63ec816df459a917c6b
k_taka <k_taka@gihyo.co.jp>
2021-07-16 10:28:13 +0900

    update.

--- docs/output-semantics-for-readme.js
+++ docs/output-semantics-for-readme.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const semantics = require('../semantics.json');
-const num = '[0-9A-Z]{1,6}([.-][0-9A-Z]{1,6}){0,6}';
+const sNumber = '[0-9A-Z]{1,6}([.-][0-9A-Z]{1,6}){0,6}';
 
 let output = '';
 for (let s of semantics) {
